cb7f33d5e981865d29c75a0af552f5ce9b241b7f
Mishania13 <mishk777@yandex.ru>
2019-10-28 21:40:29 +0300

    Initial Commit

---
+++ RGBColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBColor
+//
+//  Created by Mr. Bear on 28/10/2019.
+//  Copyright © 2019 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
