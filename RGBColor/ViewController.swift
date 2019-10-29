1d1918604fc259e82d6040b552e20c08d1eaed03
Mishania13 <mishk777@yandex.ru>
2019-10-29 22:39:42 +0300

    KeyboardAction almost done

--- RGBColor/ViewController.swift
+++ RGBColor/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textFields.forEach({doneButtonAdditing(textField: $0)})
     }
+}
 
-
+extension ViewController {
+    //Keyboard
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func any (text: UITextField) {
+        let bar = UIToolbar()
+        let reset = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(Done))
+        bar.items = [reset]
+        bar.sizeToFit()
+        text.inputAccessoryView = bar
+        
+    }
+    
+    func doneButtonAdditing(textField: UITextField) {
+        
+        let keyBoardBar = UIToolbar()
+        keyBoardBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(Done))
+        
+        let emptySpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                         target: nil,
+                                         action: nil)
+        
+        keyBoardBar.items = [emptySpace, doneButton]
+        
+        textField.inputAccessoryView = keyBoardBar
+    }
+    @objc func Done() {view.endEditing(true)}
 }
 
